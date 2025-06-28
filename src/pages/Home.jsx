c7c9c6eb9fa81c94422ad3e344fd78b7483d3173
Hrusi01 <hrusikeshkar01@gmail.com>
2025-06-29 02:06:59 +0530

    home page update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import homepage_img from "../assets/home_page_img.png";
+import "./Home.css";
 function Home() {
   return (
-    <div>
-     <img src={homepage_img} alt="Home img" />
+    <div class="hero-section">
+  <img src={homepage_img} alt="Banner" />
+    <div class="overlap-box">
+      <h2>Left Content</h2>
+      <p>This is on the left bottom over the image.</p>
     </div>
+</div>
+
   );
 }
 
