66b5fdd02f555c6086adfa2671eb802f3ca43b2b
Hrusi01 <hrusikeshkar01@gmail.com>
2025-06-28 22:50:18 +0530

    bug fix nab bar

--- src/App.jsx
+++ src/App.jsx
@@ -19,8 +19,8 @@ function App() {
         <Route path="/Pilgrim_Bazaar" element={<Pilgrim_Bazaar />} />
         <Route path="/Pilgrim_Sessions" element={<Pilgrim_Sessions />} />
         <Route path="/Pilgrim_Experiences" element={<Pilgrim_Experiences />} />
-        <Route path="/Wellness_Guide " element={<Wellness_Guide />} />
-        <Route path="/Wellness_Program " element={<Wellness_Program />} />
+        <Route path="/Wellness_Guide" element={<Wellness_Guide />} />
+        <Route path="/Wellness_Program" element={<Wellness_Program />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
     </div>
