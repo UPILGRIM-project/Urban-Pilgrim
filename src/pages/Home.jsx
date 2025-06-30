d69baf6c6de519538b1f4c37c87f78f24fe66f8f
Hrusi01 <hrusikeshkar01@gmail.com>
2025-07-01 01:58:18 +0530

    content 2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import homepage_img from "../assets/home_page_img.png";
 import homepage_overlay_img from "../assets/overlay_img.png";
+import golden_mandala from "../assets/golden-mandala.png";
 import "./Home.css";
+import Card from "../components/card.jsx";
+import cardimg1 from "../assets/card-img1.png";
+import cardimg2 from "../assets/onlinesession.png";
+
 import { easeIn, motion } from "framer-motion";
 
 function Home() {
@@ -30,7 +35,8 @@ function Home() {
             }}
           />
 
-          <motion.p className="overlaypara"
+          <motion.p
+            className="overlaypara"
             animate={{
               y: [200, 0],
               transition: { duration: 0.8 },
@@ -46,36 +52,46 @@ function Home() {
             className="divheading"
             initial={{ x: 1000 }}
             animate={{ x: -700 }}
-            transition={{ repeat: Infinity, duration: 10, ease: "linear" }}>
-           
+            transition={{ repeat: Infinity, duration: 10, ease: "linear" }}
+          >
             Explore, Heal, Transform
           </motion.div>
 
           <div className="about-container">
-            <div className="about-heading">It asks: <strong>What about me ?</strong></div>
-            <div className="aboutme-text">
-             <strong> Urban Pilgrim</strong> is a sanctuary built for that voice. A platform
-              where you don’t just find wellness—you find yourself.<br></br>Rooted in
-              India’s timeless wisdom and designed for today’s overstimulated
-              lives, we offer you a  <strong>guided path to holistic well-being.</strong>
+            <div className="about-heading">
+              It asks: <strong>What about me ?</strong>
             </div>
-           
+            <div className="aboutme-text">
+              <strong> Urban Pilgrim</strong> is a sanctuary built for that
+              voice. A platform where you don’t just find wellness—you find
+              yourself.<br></br>Rooted in India’s timeless wisdom and designed
+              for today’s overstimulated lives, we offer you a{" "}
+              <strong>guided path to holistic well-being.</strong>
+            </div>
           </div>
         </div>
       </motion.div>
-
-
-
-
-
-
-
-
-
-
-
       <div className="content2">
-        <h2>hello</h2>
+        <motion.div className="position">
+          <div className="golden_mandala">
+            <motion.img
+              animate={{ rotate: 360 }}
+              transition={{ duration: 10, repeat: Infinity, ease: "linear" }}
+              src={golden_mandala}
+              alt="noimg"
+            />
+          </div>
+        </motion.div>
+        <div className="position-content">
+          <div className="cardwrapper">
+            <Card image={cardimg1} content={"Curated experiences"}/>
+            <Card image={cardimg2} content={"Online sessions"}/>
+          </div>
+        </div>
+      </div>
+
+      <div className="demo">
+        <h2>hehehe</h2>
       </div>
     </div>
   );
